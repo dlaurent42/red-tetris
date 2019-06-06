87a055f1ce6b1ea9607969b45f3d85cf5fdfb9e1
dlaurentn <dlaurent@student.42.fr>
2019-06-06 14:54:46 +0200

    [General] Fix boilerplate eslint errors

--- src/client/index.js
+++ src/client/index.js
@@ -3,11 +3,12 @@ import ReactDom from 'react-dom'
 import createLogger from 'redux-logger'
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'                                                                                                                                                    
-import { storeStateMiddleWare } from './middleware/storeStateMiddleWare'
+import { Provider } from 'react-redux'
 import reducer from './reducers'
 import App from './containers/app'
 import { alert } from './actions/alert'
+
+// import { storeStateMiddleWare } from './middleware/storeStateMiddleWare'
 
 const initialState = {}
 
