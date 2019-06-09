d3d021ff02339d133b4685e4caf29c3fb39524ff
dlaurentn <dlaurent@student.42.fr>
2019-06-09 16:18:03 +0200

    [client] menu & router initialisation

--- client/src/components/navigation/header.jsx
+++ client/src/components/navigation/header.jsx
@@ -1,5 +1,25 @@
-/*
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-TO BE FILLED
+const header = (props) => {
 
-*/
+  // State
+  const [isLoginFormDisplayed, toggleLoginForm] = useState(false);
+  const [isRegistrationFormDisplayed, toggleRegistrationForm] = useState(false);
+
+  return (
+    <div />
+  );
+};
+
+const mapStateToProps = state => ({
+  user: state.user.user,
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(header);
--- client/src/containers/RedTetris.jsx
+++ client/src/containers/RedTetris.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
 
-const RedTetris = props => (
-  <div>
-    Red tetris
-  </div>
+const RedTetris = () => (
+  <HashRouter>
+    <Link to="/">Home</Link>
+    <Link to="/rooms">Rooms</Link>
+    <Link to="/profile">Profile</Link>
+    <Link to="/room[hello]">Specific room</Link>
+    <Switch>
+      {/* <Route path="/" exact component={} /> */}
+      <Route path="/" exact render={() => <>Home</>} />
+      <Route path="/rooms" render={() => <>In rooms</>} />
+      <Route path="/profile" render={() => <>In profile</>} />
+      <Route path="/:room[:username]" render={() => <>In room</>} />
+      <Route render={() => <>404</>} />
+    </Switch>
+  </HashRouter>
 );
+
+/*
+http://<server_name_or_ip>:<port>/#<room>[<player_name>]
+*/
 
 export default RedTetris;
