0f1b695660d3e0ec7b4013a3e556bba3dccd343c
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-06-20 19:13:53 +0200

    [server] leaderboard implemented

--- API/src/helpers/index.js
+++ API/src/helpers/index.js
@@ -2,10 +2,12 @@ import User from './User';
 import Mail from './Mail';
 import Mongo from './Mongo';
 import Router from './Router';
+import Leaderboard from './Leaderboard';
 
 export {
   User,
   Mail,
   Mongo,
   Router,
+  Leaderboard,
 };
