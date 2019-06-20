b108faf8642d70c956630411dce86a74eb2794b1
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-06-20 11:18:30 +0200

    [server] move some logic to route

--- API/src/routes/user/put.js
+++ API/src/routes/user/put.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import { get } from 'lodash';
-
 import UserHelper from '../../helpers/User';
+
+import {
+  userIsUsername,
+  userIsPassword,
+} from '../../utils';
+import { ERRORS } from '../../config/constants';
 
 const router = express.Router();
 
-router.put('/:id', (req, res) => (
-  new UserHelper(req.body.user).updateById(req.params.id)
+const validData = ({ username, password }) => (
+  !((username && !userIsUsername(username)) || (password && !userIsPassword(password)))
+);
+
+router.put('/:id', (req, res) => {
+  if (!validData(req.body.user)) {
+    return res.status(200).json({ success: false, err: ERRORS.DATA_VALIDATION });
+  }
+
+  return new UserHelper({ ...req.body.user, id: req.params.id }).updateById()
     .then(user => res.status(200).json({ success: true, user }))
-    .catch(err => res.status(200).json({ success: false, err: get(err, 'message', err) }))
-));
+    .catch(err => res.status(200).json({ success: false, err: get(err, 'message', err) }));
+});
 
 export default router;
