0b392bc9b88812fbbe2336db7aad4f75114e073c
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-06-24 21:38:11 +0200

    [API] clean of test garbage =

--- API/src/helpers/Leaderboard.unit.test.js
+++
@@ -1,9 +0,0 @@
-import getLeaderboard from './Leaderboard';
-
-describe('Leaderboard testing', () => {
-
-	test('testing ', async () => {
-		await expect(getLeaderboard()).resolves.toBe({});
-	});
-
-});
